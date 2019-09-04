bdb6a6b525cd836cbf8b8e3e26f2b1d7d29e56a2
JhonyBurbano <jhonyburbanojb@gmail.com>
2019-09-04 17:45:16 -0500

    Detalles del section y javascrit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Colgate</title>
 
     <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="style.css">
@@ -16,8 +16,43 @@
 <body>
     <main>
         <?php include('header.php'); ?>
-        
+        <section>
+            <input type="text" value="50" class="dial" id="dial">
+            <div class="articulos">
+                <img id="img_cepillos" class="img_cepillos" src="assets/img/cepillo-normal.png">
+            </div>
+            <div class="botones">
+                <img id="img_btn_1" src="assets/img/numero-01.png">
+                <img id="img_btn_2" src="assets/img/numero-02.png">
+            </div>
+        </section>
     </main>
-    
+    <script>
+        $(function() {
+            $(".dial").knob();
+        });
+
+        
+        var boton1 = document.getElementById('img_btn_1');
+        var boton2 = document.getElementById('img_btn_2');
+        var dial_input = document.getElementById('dial');
+        const img_cepillos = document.getElementById('img_cepillos');
+        boton2.style.opacity = 0.2;    
+
+        $('#img_btn_2').click(function(){
+            document.getElementById('img_cepillos').src = "assets/img/cepillo-twister.png";
+            boton1.style.opacity = 0.2;
+            boton2.style.opacity = "";
+            dial_input.value = "65";
+        });
+ 
+        $('#img_btn_1').click(function(){
+            document.getElementById('img_cepillos').src = "assets/img/cepillo-normal.png";
+            boton1.style.opacity = "";
+            boton2.style.opacity = 0.2;
+            dial_input.value = "50";
+        });
+
+    </script>
 </body>
 </html>
